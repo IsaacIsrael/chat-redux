6b8ab9fa03f8715236fed1156e79da5eb151e148
isaac israel <email.iisrael@gmail.com>
2019-04-23 17:51:09 -0300

    Finished the project

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import ChanelPanel from '../containers/chanel_panel';
+import ChanelList from '../containers/channel_list';
 
 const App = () => {
   return (
     <div className="app">
-      <p>React + Redux starter</p>
+      <div className="logo-container">
+        <img className="logo" src="assets/images/logo.svg" alt="logo" />
+      </div>
+      <ChanelList />
+      <ChanelPanel />
     </div>
   );
 };
